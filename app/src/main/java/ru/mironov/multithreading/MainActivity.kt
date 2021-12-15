36e0d97a3a33f2d08306116cf8963f32713b3add
mironoff2007 <mironoff2007@rambler.ru>
2021-12-16 01:42:22 +0300

    init

---
+++ app/src/main/java/ru/mironov/multithreading/MainActivity.kt
@@ -0,0 +1,34 @@
+package ru.mironov.multithreading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dispose = dataSource()
+            .subscribeOn(Schedulers.newThread())
+            .subscribe({
+            Log.d("My_tag", "next int $it")
+            },{
+
+            },{
+
+            })
+    }
+
+    fun dataSource(): Observable<Int> {
+        return Observable.create{ subscriber->
+            for (i in 0..100){
+                subscriber.onNext(i)
+            }
+        }
+    }
+}
